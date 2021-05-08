017fbd7efe548f0b05746d27c365a17625353255
Andrea Fiorita <andreafiorita15@gmail.com>
2021-05-08 19:49:30 +0200

    Changes to connect_db

--- connect_db.php
+++ connect_db.php
@@ -3,29 +3,32 @@
 <input type="submit" value="Post this form">
 </form>
 
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "forum";
+<?php if ($_POST['text1']): ?>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "forum";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
 
-// sql to add rows in table
-$sql = "INSERT INTO USER (user_name, user_pass, user_email, user_date, user_level)
-VALUES ('" . addslashes($_POST['text1']) . "', 'fiorita', 'andreafiorita15@gmail.com', 'unix_timestamp()', '1')";
+  // sql to add rows in table
+  $sql = "INSERT INTO USER (user_name, user_pass, user_email, user_date, user_level)
+  VALUES ('" . addslashes($_POST['text1']) . "', 'fiorita', 'andreafiorita15@gmail.com', 'unix_timestamp()', '1')";
 
 
-if (mysqli_query($conn, $sql)) {
-  echo "New record created successfully";
-} else {
-  echo "Error creating table: " . mysqli_error($conn);
-}
+  if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error creating table: " . mysqli_error($conn);
+  }
 
-mysqli_close($conn);
-?>
+  mysqli_close($conn);
+  ?>
+
+<?php endif; ?>
